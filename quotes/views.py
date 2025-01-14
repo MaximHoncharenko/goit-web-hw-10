a12755712dbd5492297540302b82c5b9fd5313c8
Max Honcharenko <157576499+MaximHoncharenko@users.noreply.github.com>
2025-01-15 00:18:14 +0200

    Add pagination to quotes display in main view

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .utils import get_mongodb
+from django.core.paginator import Paginator
 
-def main(request):
+
+def main(request, page=1):
     db = get_mongodb()
-    quotes = db.quotes.find()  # Перевірте, чи повертає запит список цитат
+    quotes = db.quotes.find()  
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page= paginator.page(page)
     return render(request, "quotes/index.html", context={"quotes": quotes})

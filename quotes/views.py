fd55de39d06a8655caf83cb058115b1f0ac5b8ad
Max Honcharenko <157576499+MaximHoncharenko@users.noreply.github.com>
2025-01-15 00:46:16 +0200

    Enhance quotes display with pagination and style updates; refactor quote retrieval in views

--- quotes/views.py
+++ quotes/views.py
@@ -5,8 +5,9 @@ from django.core.paginator import Paginator
 
 def main(request, page=1):
     db = get_mongodb()
-    quotes = db.quotes.find()  
+    quotes = list(db.quotes.find())  
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page= paginator.page(page)
     return render(request, "quotes/index.html", context={"quotes": quotes})
+    print(list(quotes))  # Це виведе всі цитати на консоль

b147e5d094df60452c7396cff6c0f811702796a5
Max Honcharenko <157576499+MaximHoncharenko@users.noreply.github.com>
2025-01-15 16:01:41 +0200

    Implement pagination for quotes display and add tag-based filtering; refactor views and templates

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-
-app_name = "quotes"
-
 urlpatterns = [
-    path("", views.main, name="root"),
+    path('', views.main, name='main'),  # Головна сторінка
+    path('tag/<str:tag>/', views.quotes_by_tag, name='quotes_by_tag'),  # Пошук за тегом
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .utils import get_mongodb
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def main(request, page=1):
+def main(request):
     db = get_mongodb()
-    quotes = list(db.quotes.find())  
-    per_page = 10
-    paginator = Paginator(list(quotes), per_page)
-    quotes_on_page= paginator.page(page)
-    return render(request, "quotes/index.html", context={"quotes": quotes})
-    print(list(quotes))  # Це виведе всі цитати на консоль
+    quotes = list(db.quotes.find())  # Отримуємо всі цитати
+    per_page = 10  # Кількість цитат на сторінку
+
+    paginator = Paginator(quotes, per_page)
+    page = request.GET.get('page', 1)  # Номер сторінки
+    try:
+        quotes_on_page = paginator.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        quotes_on_page = paginator.page(1)
+
+    return render(request, "quotes/index.html", context={"quotes_on_page": quotes_on_page})
+
+
+def quotes_by_tag(request, tag):
+    db = get_mongodb()
+    quotes = list(db.quotes.find({"tags": tag}))  # Знайти цитати з конкретним тегом
+    return render(request, "quotes/quotes_by_tag.html", context={"quotes": quotes, "tag": tag})

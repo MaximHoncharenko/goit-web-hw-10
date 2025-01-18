abe8ce66ff515e86aefeaad04e0d0838f8c96899
Max Honcharenko <157576499+MaximHoncharenko@users.noreply.github.com>
2025-01-18 15:16:35 +0200

    final

--- myproject/settings.py
+++ myproject/settings.py
@@ -66,10 +66,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '0997943465',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
+
 
 
 # Password validation
